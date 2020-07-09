c358684388b5aab45249573a94745708bfa2bf87
Shruti0398 <shrusp98@gmail.com>
2020-07-09 19:21:39 +0530

    media page with responsive

--- media.php
+++ media.php
@@ -1,5 +1,116 @@
 <?php include 'header.php';?>
 
-media page
+<!-- banner section start -->
+
+<section class="banner-section" id="ban-id">
+    <div class="media-bann">
+        <div class="bannercon-txt custom-heading">
+            <h5 class="text-uppercase white text-center">Lorem ipsum</h5>
+            <h2 class="text-uppercase white text-center">Lorem ipsum</h2>
+        </div>
+    </div>
+</section>
+
+<!-- banner section ends -->
+
+<!-- contact us start -->
+
+<div class="con-page">
+    <div class="container pdng-top">
+        <div class="cutsom-heading">
+            <h2 class="text-left text-uppercase txt-color" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1500">media</h2>
+        </div>
+        <div class="green-line prtnr-line" data-aos="fade-left" data-aos-easing="linear" data-aos-duration="1500"></div>
+        <div class="row custom-row-setting">
+            <div class="col-lg-3 col-md-3 col-12 custom-col-setting">
+                <div class="view-button view-button-active" id="ongoing">
+                    <a class="text-uppercase" id="ong">what's new</a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-12 custom-col-setting">
+                <div class="view-button" id="delivered">
+                    <a class="text-uppercase" id="del">past events</a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-12 custom-col-setting">
+                <div class="view-button" id="delivered">
+                    <a class="text-uppercase" id="del">news</a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-12 custom-col-setting">
+                <div class="view-button" id="delivered">
+                    <a class="text-uppercase" id="del">press release</a>
+                </div>
+            </div>
+        </div>
+        <div class="press pdng-top">
+            <h5 class="text-uppercase">press impressions</h5>
+        </div>
+        <div class="row media-card pdng-top">
+            <div class="col-lg-4 col-md-4 col-12">
+                <div class="card">
+                    <img class="card-img-top" src="img/media_page/Media-01.png" alt="Card image">
+                    <div class="card-body">
+                        <p class="card-title txt-color">06-Jul-2020</p>
+                        <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+                <div class="card">
+                    <img class="card-img-top" src="img/media_page/Media-02.png" alt="Card image">
+                    <div class="card-body">
+                        <p class="card-title txt-color">06-Jul-2020</p>
+                        <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+                <div class="card">
+                    <img class="card-img-top" src="img/media_page/Media-03.png" alt="Card image">
+                    <div class="card-body">
+                        <p class="card-title txt-color">06-Jul-2020</p>
+                        <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row media-card pdng-top pdng-btm-120">
+            <div class="col-lg-4 col-md-4 col-12">
+                <div class="card">
+                    <img class="card-img-top" src="img/media_page/Media-04.png" alt="Card image">
+                    <div class="card-body">
+                        <p class="card-title txt-color">06-Jul-2020</p>
+                        <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+                <div class="card">
+                    <img class="card-img-top" src="img/media_page/Media-05.png" alt="Card image">
+                    <div class="card-body">
+                        <p class="card-title txt-color">06-Jul-2020</p>
+                        <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+                <div class="card">
+                    <img class="card-img-top" src="img/media_page/Media-01.png" alt="Card image">
+                    <div class="card-body">
+                        <p class="card-title txt-color">06-Jul-2020</p>
+                        <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="view-button load-media" id="delivered">
+            <a class="text-uppercase" id="del">load more</a>
+        </div>
+    </div>
+    <div class="clearfix"></div>
+</div>
+
+<!-- contact us ends -->
 
 <?php include 'footer.php';?>
