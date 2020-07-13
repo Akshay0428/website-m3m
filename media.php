41360b8e5d9b03275894f81a38d247901810a0ec
Shruti0398 <shrusp98@gmail.com>
2020-07-13 12:14:50 +0530

    friday - media section mobile and desktop

--- media.php
+++ media.php
@@ -23,89 +23,136 @@
         <div class="green-line prtnr-line" data-aos="fade-left" data-aos-easing="linear" data-aos-duration="1500"></div>
         <div class="row custom-row-setting">
             <div class="col-lg-3 col-md-3 col-12 custom-col-setting">
-                <div class="view-button view-button-active" id="ongoing">
-                    <a class="text-uppercase" id="ong">what's new</a>
+                <div class="view-button view-button-active" id="med1">
+                    <a class="text-uppercase" id="medhov1">what's new</a>
                 </div>
             </div>
             <div class="col-lg-3 col-md-3 col-12 custom-col-setting">
-                <div class="view-button" id="delivered">
-                    <a class="text-uppercase" id="del">past events</a>
+                <div class="view-button" id="med2">
+                    <a class="text-uppercase" id="medhov2">past events</a>
                 </div>
             </div>
             <div class="col-lg-3 col-md-3 col-12 custom-col-setting">
-                <div class="view-button" id="delivered">
-                    <a class="text-uppercase" id="del">news</a>
+                <div class="view-button" id="med3">
+                    <a class="text-uppercase" id="medhov3">news</a>
                 </div>
             </div>
             <div class="col-lg-3 col-md-3 col-12 custom-col-setting">
-                <div class="view-button" id="delivered">
-                    <a class="text-uppercase" id="del">press release</a>
+                <div class="view-button" id="med4">
+                    <a class="text-uppercase" id="medhov4">press release</a>
                 </div>
             </div>
         </div>
         <div class="press pdng-top">
             <h5 class="text-uppercase">press impressions</h5>
         </div>
-        <div class="row media-card pdng-top">
-            <div class="col-lg-4 col-md-4 col-12">
-                <div class="card">
-                    <img class="card-img-top" src="img/media_page/Media-01.png" alt="Card image">
-                    <div class="card-body">
-                        <p class="card-title txt-color">06-Jul-2020</p>
-                        <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+        <div id="media-box-1">
+            <div class="row media-card pdng-top">
+                <div class="col-lg-4 col-md-4 col-12">
+                    <div class="card">
+                        <img class="card-img-top" src="img/media_page/Media-01.png" alt="Card image">
+                        <div class="card-body">
+                            <p class="card-title txt-color">06-Jul-2020</p>
+                            <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <div class="card">
+                        <img class="card-img-top" src="img/media_page/Media-02.png" alt="Card image">
+                        <div class="card-body">
+                            <p class="card-title txt-color">06-Jul-2020</p>
+                            <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <div class="card">
+                        <img class="card-img-top" src="img/media_page/Media-03.png" alt="Card image">
+                        <div class="card-body">
+                            <p class="card-title txt-color">06-Jul-2020</p>
+                            <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-4 col-12">
-                <div class="card">
-                    <img class="card-img-top" src="img/media_page/Media-02.png" alt="Card image">
-                    <div class="card-body">
-                        <p class="card-title txt-color">06-Jul-2020</p>
-                        <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+            <div class="row media-card pdng-top">
+                <div class="col-lg-4 col-md-4 col-12">
+                    <div class="card">
+                        <img class="card-img-top" src="img/media_page/Media-04.png" alt="Card image">
+                        <div class="card-body">
+                            <p class="card-title txt-color">06-Jul-2020</p>
+                            <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <div class="card">
+                        <img class="card-img-top" src="img/media_page/Media-05.png" alt="Card image">
+                        <div class="card-body">
+                            <p class="card-title txt-color">06-Jul-2020</p>
+                            <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <div class="card">
+                        <img class="card-img-top" src="img/media_page/Media-01.png" alt="Card image">
+                        <div class="card-body">
+                            <p class="card-title txt-color">06-Jul-2020</p>
+                            <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-4 col-12">
-                <div class="card">
-                    <img class="card-img-top" src="img/media_page/Media-03.png" alt="Card image">
-                    <div class="card-body">
-                        <p class="card-title txt-color">06-Jul-2020</p>
-                        <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+            <!-- extra sections  -->
+            <div id="section-more">
+                <!-- <div class="article"> -->
+                    <div class="moretext">
+                        <div class="row media-card pdng-top">
+                            <div class="col-lg-4 col-md-4 col-12">
+                                <div class="card">
+                                    <img class="card-img-top" src="img/media_page/Media-01.png" alt="Card image">
+                                    <div class="card-body">
+                                        <p class="card-title txt-color">06-Jul-2020</p>
+                                        <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-md-4 col-12">
+                                <div class="card">
+                                    <img class="card-img-top" src="img/media_page/Media-02.png" alt="Card image">
+                                    <div class="card-body">
+                                        <p class="card-title txt-color">06-Jul-2020</p>
+                                        <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-md-4 col-12">
+                                <div class="card">
+                                    <img class="card-img-top" src="img/media_page/Media-03.png" alt="Card image">
+                                    <div class="card-body">
+                                        <p class="card-title txt-color">06-Jul-2020</p>
+                                        <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                <!-- </div> -->
+                <div class="view-button" id="med4">
+                    <a class="text-uppercase moreless-button" id="">load more</a>
                 </div>
             </div>
         </div>
-        <div class="row media-card pdng-top pdng-btm-120">
-            <div class="col-lg-4 col-md-4 col-12">
-                <div class="card">
-                    <img class="card-img-top" src="img/media_page/Media-04.png" alt="Card image">
-                    <div class="card-body">
-                        <p class="card-title txt-color">06-Jul-2020</p>
-                        <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-4 col-12">
-                <div class="card">
-                    <img class="card-img-top" src="img/media_page/Media-05.png" alt="Card image">
-                    <div class="card-body">
-                        <p class="card-title txt-color">06-Jul-2020</p>
-                        <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-4 col-12">
-                <div class="card">
-                    <img class="card-img-top" src="img/media_page/Media-01.png" alt="Card image">
-                    <div class="card-body">
-                        <p class="card-title txt-color">06-Jul-2020</p>
-                        <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
-                    </div>
-                </div>
-            </div>
+        <div id="media-box-2">
+            <p>Media 2</p>
         </div>
-        <div class="view-button load-media" id="delivered">
-            <a class="text-uppercase" id="del">load more</a>
+        <div id="media-box-3">
+        <p>Media 3</p>
+        </div>
+        <div id="media-box-4">
+        <p>Media 4</p>
         </div>
     </div>
     <div class="clearfix"></div>
