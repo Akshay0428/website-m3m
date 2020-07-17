c070d004f5ad2efa32f324e03586e8a98567ac58
rajkumar <rajkumar.remalli@gmail.com>
2020-07-17 15:36:40 +0530

    redefining corrections

--- redefining.php
+++ redefining.php
@@ -1,5 +1,7 @@
 <?php include 'header.php';?>
 
+
+
 <!-- banner section start -->
 
 <section class="banner-section" id="ban-id">
@@ -25,15 +27,15 @@
                     <p data-aos="zoom-in" data-aos-easing="linear" data-aos-duration="1500">Gurugram, the cradle of the new age civilization, the unrivalled cosmopolitan capital of India is a city of youth and dreams. A vibrant market where wealth is created and multiplied and the very air is ripe with opportunities, this city will soon be a landscape benchmark in India. New Gurugram, the next growth corridor of the city, is touted to be the epicentre of luxury and the new power spot of Gurugram. It is strategically located and has smooth connectivity within and outside of Gurugram. It is fast becoming the cynosure of the customer’s eyes and a sought after destination for investment.</p>
                 </div>
             </div>
-            <div class="d_hide">
+            <div class="d_hide col-lg-12 col-md-12 col-12">
                 <h2 class="text-uppercase txt-color" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1500">redefining gurugram</h2>
                 <div class="green-line prtnr-line" data-aos="zoom-in" data-aos-easing="linear" data-aos-duration="1500"></div>
             </div>
             <div class="col-lg-6 col-md-6 col-12">
                 <img src="img/redefining/redef_txt.png" alt="refef image">
             </div>
-            <p data-aos="zoom-in" data-aos-easing="linear" data-aos-duration="1500" class="d_hide">Gurugram, the cradle of the new age civilization, the unrivalled cosmopolitan capital of India is a city of youth and dreams. A vibrant market where wealth is created and multiplied and the very air is ripe with opportunities, this city will soon be a landscape benchmark in India. New Gurugram, the next growth corridor of the city, is touted to be the epicentre of luxury and the new power spot of Gurugram. It is strategically located and has smooth connectivity within and outside of Gurugram. It is fast becoming the cynosure of the customer’s eyes and a sought after destination for investment.</p>
-            <p class="pdng-top" data-aos="zoom-in" data-aos-easing="linear" data-aos-duration="1500">We, at M3M, are determined to transform this new growth corridor into one of the best places to be in. With our commitment towards excellence in quality and service and a promise of timely delivery, we are geared up for this new chapter in the real estate industry.</p>
+            <p class="d_hide col-lg-12 col-md-12 col-12" data-aos="zoom-in" data-aos-easing="linear" data-aos-duration="1500" class="d_hide">Gurugram, the cradle of the new age civilization, the unrivalled cosmopolitan capital of India is a city of youth and dreams. A vibrant market where wealth is created and multiplied and the very air is ripe with opportunities, this city will soon be a landscape benchmark in India. New Gurugram, the next growth corridor of the city, is touted to be the epicentre of luxury and the new power spot of Gurugram. It is strategically located and has smooth connectivity within and outside of Gurugram. It is fast becoming the cynosure of the customer’s eyes and a sought after destination for investment.</p>
+            <p class="col-lg-12 col-md-12 col-12" data-aos="zoom-in" data-aos-easing="linear" data-aos-duration="1500">We, at M3M, are determined to transform this new growth corridor into one of the best places to be in. With our commitment towards excellence in quality and service and a promise of timely delivery, we are geared up for this new chapter in the real estate industry.</p>
         </div>
     </div>
 </div>
